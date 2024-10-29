af710b4e997bb1a46a60a5af54405bf4f16c8622
Sharon Griffith <sagriffith2021@gmail.com>
2024-10-29 01:32:26 -0600

    store Clerk users in database

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+//domain: "https://vibrant-goldfinch-406.convex.site",
+//domain: "https://lasting-mayfly-25.clerk.accounts.dev",
 export default {
   providers: [
     {
